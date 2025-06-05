db2837f5a556d7e194055151fe1ec88bb39b7b4b
MR. YATISH <yatishgottapu2026@gmail.com>
2025-06-05 08:30:41 +0530

    Initial commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { assets, songsData } from '../assets/assets'
+const Player = () => {
+  return (
+    <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
+        <div className='hidden lg:flex items-center gap-4'>
+            <img className='w-12' src={songsData[0].image} alt="" />
+            <div>
+                <p>{songsData[0].name}</p>
+                <p>{songsData[0].desc.slice(0,12)}</p>
+
+            </div>
+        </div>
+        <div className='flex -col items-center gap-1 m-auto'>
+            <div className='flex gap-4'>
+                <img src={assets.shuffle_icon} alt="" className='w-4 cursor-pointer' />
+                <img src={assets.prev_icon} alt="" className='w-4 cursor-pointer' />
+                <img src={assets.play_icon} alt="" className='w-4 cursor-pointer' />
+                <img src={assets.next_icon} alt="" className='w-4 cursor-pointer' />
+                <img src={assets.loop_icon} alt="" className='w-4 cursor-pointer' />
+
+            </div>
+            <div className='flex items-center gap-5'>
+                <p>1:06</p>
+                <div className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
+                    <hr className='h-1 border-none w-10 bg-green-800 rounded-full'/>
+                </div>
+                <p>3:20</p>
+
+
+            </div>
+
+        </div>
+        
+    </div>
+  )
+}
+
+export default Player
