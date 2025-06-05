bc787df8e9df1e32c6059bf4ac6c9a5373cd4155
MR. YATISH <yatishgottapu2026@gmail.com>
2025-06-05 20:27:04 +0530

    created Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,6 +16,12 @@ const Navbar = () => {
             <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center'>Dia</p>
         </div>
     </div>
+    <div className='flex items-center gap-2 mt-4'>
+      <p className='bg-white text-black px-4 rounded-2xl cursor-pointer'>all</p>
+      <p className='bg-green-300 text-white px-4 py-1 rounded-2xl cursor-pointer'>music</p>
+      <p className='bg-green-300 text-white px-4 py-1 rounded-2xl cursor-pointer'>podcasts</p>
+
+    </div>
     </>
     
 
