eac8f987f9f6ba52a584753190d2e785697439cb
MR. YATISH <yatishgottapu2026@gmail.com>
2025-06-06 09:38:10 +0530

    ui part completed display,displayalbum,displayhome

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ const App = () => {
 
       </div>
       <Player/>
+      <audio preload='auto'></audio>
 
     </div>
   )
--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
-import Navbar from './Navbar'
-import { Route } from 'react-router-dom'    
+import React from 'react';
+import Navbar from './Navbar';
+import AlbumItem from './AlbumItem';
+import SongItem from './SongItem';
+import { albumsData, songsData } from '../assets/assets';
 
 const DisplayHome = () => {
   return (
     <>
-    <Navbar />
-    </>
-  )
-}
+      <Navbar />
+      <div className='p-4 text-white'> {/* white text and padding */}
+        <div className='mb-8'>
+          <h1 className='my-5 font-bold text-2xl'>Featured Charts</h1>
+          <div className='flex overflow-auto gap-4'>
+            {albumsData.map((item, index) => (
+              <AlbumItem
+                key={index}
+                name={item.name}
+                image={item.image}
+                desc={item.desc}
+                id={item.id}
+              />
+            ))}
+          </div>
+        </div>
 
-export default DisplayHome
+        <div className='mb-8'>
+          <h1 className='my-5 font-bold text-2xl'>Today's Biggest Hits</h1>
+          <div className='flex overflow-auto gap-4'>
+            {songsData.map((item, index) => (
+              <SongItem
+                key={index}
+                name={item.name}
+                image={item.image}
+                desc={item.desc}
+                id={item.id}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DisplayHome;
