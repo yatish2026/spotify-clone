65ea7eae3fbab500ac977d675c41a3e261be33d4
MR. YATISH <yatishgottapu2026@gmail.com>
2025-06-06 23:53:55 +0530

    fixed git deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/spotify-clone/', // 👈 Add this line
+    base: '/SPOTIFY-CLONE/', // IMPORTANT for GitHub Pages
     plugins: [react()],
 })
