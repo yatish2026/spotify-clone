dc4b1d546780139a33c839df80b554f4dc1b2bd0
MR. YATISH <yatishgottapu2026@gmail.com>
2025-06-08 07:58:32 +0530

    Added Spotify logo to browser tab

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from "react-router-dom";
+
 import PlayerContextProvider from './context/PlayerContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <PlayerContextProvider>
         <App />
       </PlayerContextProvider>
-    </BrowserRouter>
-    
+    </HashRouter>
+
   </StrictMode>,
 )
